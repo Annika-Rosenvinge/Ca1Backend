03f8d533f5f40f45b21d340cc4be72014a2781b2
Annika Rosenvinge Jespersen <Annika2000@live.dk>
2022-03-19 12:42:18 +0100

    Update CorsFilter.java

--- src/main/java/cors/CorsFilter.java
+++ src/main/java/cors/CorsFilter.java
@@ -9,20 +9,28 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
+    private static boolean preFlightRequest(ContainerRequestContext containerRequestContext){
+        return containerRequestContext.getHeaderString("Origin") !=null && containerRequestContext.getMethod().equalsIgnoreCase("OPTIONS");
+    }
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext,
                        ContainerResponseContext containerResponseContext) throws IOException {
-        containerRequestContext.getHeaders().add(
-                "Access-Control-Allow-Origin", "*");
-        containerRequestContext.getHeaders().add(
-                "Access-Control-Allow-Credentials", "true");
-        containerResponseContext.getHeaders().add(
-                "Access-Control-Allow-Headers",
-                "origin, content-type, accept, authorization");
-        containerResponseContext.getHeaders().add(
-                "Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
+        if (containerRequestContext.getHeaderString("Origin") == null){
+            return;
+        }
+        if (preFlightRequest(containerRequestContext)) {
+            containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+            // Whatever other non-standard/safe headers (see list above)
+            // you want the client to be able to send to the server,
+            // put it in this list (v:). And remove the ones you don't want.
+            containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization,x-access-token");
+
+        }
+        //This one must always be present
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
     }
 }
 
