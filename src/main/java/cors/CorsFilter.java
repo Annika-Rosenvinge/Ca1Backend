d46fb0abeaef99d7700bba54236d1205cbf1023b
Annika Rosenvinge Jespersen <Annika2000@live.dk>
2022-03-20 14:19:25 +0100

    update cors

--- src/main/java/cors/CorsFilter.java
+++ src/main/java/cors/CorsFilter.java
@@ -2,16 +2,30 @@ package cors;
 
 import java.io.IOException;
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class CorsFilter implements ContainerResponseFilter {
+public class CorsFilter implements ContainerResponseFilter, ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext request) throws IOException {
+
+        //If it's a preflight request, abort the request with a 200 status, and the CORS headers are added in the
+        // response filter method below.
+        if (preFlightRequest(request)) {
+            request.abortWith(Response.ok().build());
+            return;
+        }
+    }
 
     private static boolean preFlightRequest(ContainerRequestContext containerRequestContext){
         return containerRequestContext.getHeaderString("Origin") !=null && containerRequestContext.getMethod().equalsIgnoreCase("OPTIONS");
     }
+
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext,
@@ -19,11 +33,11 @@ public class CorsFilter implements ContainerResponseFilter {
         //This one must always be present
         containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
 
-        containerRequestContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        //containerRequestContext.getHeaders().add("Access-Control-Allow-Origin", "*");
 
-        /*if (containerRequestContext.getHeaderString("Origin") == null){
+        if (containerRequestContext.getHeaderString("Origin") == null){
             return;
-        }*/
+        }
         if (preFlightRequest(containerRequestContext)) {
             containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
             containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
